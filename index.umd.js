06fce1915904941a83eae8c5da7b54dd7531aaef
Marco Oliveira <marcooliveira@me.com>
2016-11-30 21:43:05 +0000

    [fix] browserify

--- index.umd.js
+++ index.umd.js
@@ -1,6 +1,8 @@
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.onTransitionEnd = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
 
+var longestTransition = require('longest-transition');
+
 var defaultEventFailureGracePeriod = 100;
 
 // transitionend below based on https://github.com/kubens/transition-utility
@@ -16,10 +18,8 @@ var transitionend = (function () {
 
     // look for supported transition
     for (transition in transitions) {
-        if (transitions.hasOwnProperty(transition)) {
-            if (element.style[transition] !== undefined) {
-                return transitions[transition];
-            }
+        if (element.style[transition] !== undefined) {
+            return transitions[transition];
         }
     }
 
@@ -27,45 +27,87 @@ var transitionend = (function () {
     return false;
 })();
 
-module.exports = function (element, expectedDuration, callback) {
+module.exports = function (element, options, callback) {
+    var timeout;
     var gracePeriod;
-    var timeOutTimer;
+    var timeoutTimer;
+    var longest;
 
     // browser does not support transitionend, no animation
     if (!transitionend) {
-        setTimeout(function __handleSetTimeout() {
-            callback();
-        }, 0);
+        setTimeout(callback, 0);
 
         return;
     }
 
-    // define the duration and grace period
-    if (typeof expectedDuration === 'object') {
-        gracePeriod = expectedDuration.gracePeriod;
-        expectedDuration = expectedDuration.duration;
-    } else {
-        gracePeriod = defaultEventFailureGracePeriod;
+    // handle default parameters
+    if (typeof options === 'function') {
+        callback = options;
+        options = {};
     }
+
+    // if no timeout provided, infer it
+    if (!options.timeout) {
+        longest = longestTransition(element);
+    }
+
+    timeout = options.timeout || (longest.duration + longest.delay);
+    gracePeriod = options.gracePeriod || defaultEventFailureGracePeriod;
 
     element.addEventListener(transitionend, __handleTransitionEnd);
 
-    timeOutTimer = setTimeout(__handleTransitionEnd, expectedDuration + gracePeriod);
+    timeoutTimer = setTimeout(__handleTransitionEnd, timeout + gracePeriod);
 
     function __handleTransitionEnd(e) {
-        // if event is on target
-        if (e.target === element) {
+        // if event timed out or it is on target (respecting the longest transitioning property if necessary)
+        if (!e || (e.target === element && (!longest || longest.property === e.propertyName))) {
             // clear the timer if it's still running
-            clearTimeout(timeOutTimer);
+            clearTimeout(timeoutTimer);
 
             element.removeEventListener(transitionend, __handleTransitionEnd);
 
             if (callback) {
-                callback(e);
+                callback();
             }
         }
     }
 };
+
+},{"longest-transition":2}],2:[function(require,module,exports){
+'use strict';
+
+function __readTransitions(el) {
+    var styles = getComputedStyle(el);
+    var properties = styles.transitionProperty.replace(' ', '').split(',');
+    var durations = styles.transitionDuration.replace(' ', '').split(',').map(_parseFloat);
+    var delays = styles.transitionDelay.replace(' ', '').split(',').map(_parseFloat);
+
+    return properties.map(function __handleMapProperties(prop, i) {
+        return {
+            property: prop,
+            duration: Math.ceil((durations[i] || durations[0] || 0) * 1000),
+            delay: Math.ceil((delays[i] || delays[0] || 0) * 1000),
+        };
+    });
+}
+
+function longestTransition(el) {
+    var longest = __readTransitions(el).reduce(function __handleReduceTransitions(prev, curr) {
+        if (!prev) {
+            return curr;
+        }
+
+        return prev.duration + prev.delay >= curr.duration + curr.delay ? prev : curr;
+    }, null);
+
+    return longest;
+}
+
+function _parseFloat(x) {
+    return parseFloat(x, 10);
+}
+
+module.exports = longestTransition;
 
 },{}]},{},[1])(1)
 });
